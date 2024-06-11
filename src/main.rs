d616e82f5847411da9f941500a758e8c324e4f96
Clément Hurlin <clement.hurlin@tweag.io>
2024-06-11 15:37:43 +0200

    Add Room trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+#[derive(Debug, Eq, PartialEq)]
+pub struct Coord {
+    x: i32,
+    y: i32, // Recall that smaller y means more at the top.
+}
+
+impl Coord {
+    /// Creates a new [`Coord`].
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+}
+
+pub trait Room {
+    fn to_coords(&self) -> Vec<Coord>;
+}
+
+impl Room for Coord {
+    fn to_coords(&self) -> Vec<Coord> {
+        let mut v = Vec::new();
+        let copy = Coord::new(self.x, self.y);
+        v.push(copy);
+        return v;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Coord;
+    use crate::Room;
+
+    #[test]
+    fn coord_to_coords() {
+        let c = Coord::new(0, 0);
+        let coords = c.to_coords();
+        let coords_head = coords.get(0);
+        match coords_head {
+            None => panic!("Coord.to_coords should return a non-empty list"),
+            Some(head) => assert_eq!(c, *head),
+        }
+    }
+}
+
 fn main() {
     println!("Generating a dungeon for you my lord");
 
@@ -14,5 +57,4 @@ fn main() {
         println!("{}", inside2);
     }
     println!("{}", header);
-
 }
