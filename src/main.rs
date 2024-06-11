a1c99d8c69babfb2ea1ed28e7b34ab114fde9fdb
Clément Hurlin <clement.hurlin@tweag.io>
2024-06-11 13:53:19 +0200

    Draw walls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Generating a dungeon for you my lord");
+
+    let width = 80;
+    let height = 50;
+
+    let header = "#".repeat(width);
+    let inside = " ".repeat(width - 2);
+    let inside2 = format!("#{inside}#");
+
+    // Print the enclosing walls
+    println!("{}", header);
+    for _i in 0..height - 2 {
+        println!("{}", inside2);
+    }
+    println!("{}", header);
+
 }
